27c3c67f170834118be28b35ac6933cdca7a1259
Hajime <114803667+hajimeyudha@users.noreply.github.com>
2023-02-28 16:45:45 +0700

    duh pusing

---
+++ resources/views/layouts/main-layout-cssjs.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta name="description" content="" />
+  <meta name="author" content="Template Mo" />
+  <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet" />
+
+  <title>PT Subur Makmur Migas Pratama</title>
+  <!-- Additional CSS Files -->
+  <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" />
+  <link rel="stylesheet" type="text/css" href="assets/css/create.css" />
+  <!-- <link rel="stylesheet" type="text/css" href="assets/css/templatemo-art-factory.css"> -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" />
+</head>
